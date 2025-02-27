a9cb0bf48dafd53fa7d5582fbc3ce7d815414d30
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-02-27 12:31:27 +0000

    Update configuration.ts

--- src/Electron/Configuration/configuration.ts
+++ src/Electron/Configuration/configuration.ts
@@ -24,7 +24,11 @@ export function writeConfig(config: Config): void {
 }
 
 export function writeConfigSync(config: Config): void {
-    const configFile = path.join(app.getPath('appData'), app.getName(), 'Configuration', 'config.json')
+    const configFolder = path.join(app.getPath('appData'), app.getName(), 'Configuration')
+    const configFile = path.join(configFolder, 'config.json')
+
+    if (!fs.existsSync(configFolder))
+        fs.mkdirSync(configFolder, { recursive: true })
 
     fs.writeFileSync(configFile, JSON.stringify(config))
 }
